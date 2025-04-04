569ff7423036d6c7ae7ee9f7555865c839e4eb5d
apocalypse55 <shardool22rtipathi@gmail.com>
2025-04-04 14:57:29 +0545

    Feat: loaded and updated data

---
+++ src/main/java/com/shardool/liquibase_demo/LiquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shardool.liquibase_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shardool/liquibase_demo/LiquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shardool.liquibase_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
